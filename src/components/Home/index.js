be65fdcd6aea2e3051863d1ee28a82ad15ce8b3e
keithpaul <keith_paul@outlook.com>
2019-10-29 11:31:15 +1100

    changed home text

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,8 +7,10 @@ export default class Home extends React.Component {
     render(){
         return (
             <div className="home">
-                <h3>Space</h3>
-                <p>This site is dedicated to showing the best and most amazing pictures of space and the stars. </p>
+                <h3 className="spaceTitle">Space<br /><span>/speɪs/</span></h3>
+                <p className="noun">noun</p>
+                <p className="description">1. a continuous area or expanse which is free, available, or unoccupied</p>
+                <p className="description">2. the dimensions of height, depth, and width within which all things exist and move.</p>
                 <p><Link to="/apod">APOD</Link> stands for Astronomy Picture of the Day and connects to the NASA APOD API showcasing their photos of the day</p>
             </div>
         );
